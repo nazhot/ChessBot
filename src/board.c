d3b502eb83663aabafd317a97c9219cb6b04f416
nazhot <15216154+nazhot@users.noreply.github.com>
2023-12-19 14:53:09 -0700

    Initializing the board

--- src/board.c
+++ src/board.c
@@ -1,8 +1,8 @@
+#include <assert.h>
+#include "board.h"
+#include "pieces.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include "board.h"
-
-
 
 struct Board* board_initialize() {
     struct Board *board = malloc( sizeof( struct Board ) );
@@ -13,6 +13,24 @@ struct Board* board_initialize() {
     
     board->whiteToMove = true;
     board->bitMap = 0b1111111111111111000000000000000000000000000000001111111111111111;
+    //need to find way to distinguish black vs. white
+    board->pieceMap[0][0] = ROOK;
+    board->pieceMap[0][1] = KNIGHT;
+    board->pieceMap[0][2] = BISHOP;
+    board->pieceMap[0][3] = QUEEN;
+    board->pieceMap[0][4] = KING;
+    board->pieceMap[0][5] = BISHOP;
+    board->pieceMap[0][6] = KNIGHT;
+    board->pieceMap[0][7] = ROOK;
+
+    board->pieceMap[7][0] = ROOK;
+    board->pieceMap[7][1] = KNIGHT;
+    board->pieceMap[7][2] = BISHOP;
+    board->pieceMap[7][3] = KING;
+    board->pieceMap[7][4] = QUEEN;
+    board->pieceMap[7][5] = BISHOP;
+    board->pieceMap[7][6] = KNIGHT;
+    board->pieceMap[7][7] = ROOK;
 
     return board;
 }
--- src/board.h
+++ src/board.h
@@ -6,10 +6,15 @@
 
 struct Board {
     int64_t bitMap; //what parts of the board are occupied, each square is 1 bit
-    int64_t pieceMap[3]; //actual pieces on the board
+    unsigned char pieceMap[8][8]; //actual pieces on the board
     bool    whiteToMove; 
 }; 
 
+/**
+ * Initialize board with the standard starting layout
+ *
+ * @return newly created Board
+ */
 struct Board* board_initialize();
 
 
